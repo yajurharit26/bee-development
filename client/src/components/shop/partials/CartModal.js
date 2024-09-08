e59842022d52a1e672024fc8508a63f9cb405e3f
Yajat Dureja <75976299+Yajat047@users.noreply.github.com>
2024-09-09 00:46:13 +0530

    CART API URL

--- client/src/components/shop/partials/CartModal.js
+++ client/src/components/shop/partials/CartModal.js
@@ -6,7 +6,7 @@ import { isAuthenticate } from "../auth/fetchApi";
 import { cartList } from "../productDetails/Mixins";
 import { subTotal, quantity, totalCost } from "./Mixins";
 
-const apiURL = process.env.REACT_APP_API_URL;
+const apiURL = "https://bee-development.onrender.com";
 
 const CartModal = () => {
   const history = useHistory();
